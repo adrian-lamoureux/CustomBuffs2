33bb426c4a6e2af91a986307db2442d8f82bb66b
Adrian Lamoureux <phail.edu@gmail.com>
2021-07-04 17:48:42 -0400

    new independent options window

--- CustomBuffs.lua
+++ CustomBuffs.lua
@@ -3502,18 +3502,62 @@ function CustomBuffs:loaded()
 
 end
 
+function createOptionsTab(self, container)
+	--local frame = self.gui:Create("InlineGroup");
+	local tab1 = self.gui:Create("Label");
+	tab1:SetFullWidth(true);
+	--frame:SetFullWidth(true);
+
+	self.dialog:Open("CustomBuffs", container);
+	tab1:SetText("");
+	--tab1:AddChild(frame);
+	container:AddChild(tab1);
+end
+
+function createProfilesTab(self, container)
+	--local frame = self.gui:Create("InlineGroup");
+	local tab2 = self.gui:Create("Label");
+	self.dialog:Open("CustomBuffs Profiles", container);
+	tab2:SetFullWidth(true);
+	tab2:SetText("");
+	--frame:SetFullWidth(true);
+	--tab2:AddChild(frame);
+	container:AddChild(tab2);
+end
+
 function CustomBuffs:OpenOptions()
 	if not CustomBuffs.optionsOpen then
+
+
+
+
 		CustomBuffs.optionsOpen = true;
 		local frame = self.gui:Create("Window");
-		frame:SetWidth(3.45);
+		frame:SetLayout("Fill");
+		frame:SetWidth(700);
 		frame:SetTitle("CustomBuffs2 Options");
 		frame:SetCallback("OnClose", function(widget)
 			self.gui:Release(widget);
 			CustomBuffs.optionsOpen = false;
 		end);
 
-		self.dialog:Open("CustomBuffs", frame);
+		local tab =  self.gui:Create("TabGroup");
+		tab:SetLayout("Flow");
+		tab:SetCallback("OnGroupSelected", function(container, event, group)
+			--if CustomBuffs.verbose then print(container); end
+			container:ReleaseChildren();
+   			if group == "tab1" then
+      			createOptionsTab(self, container);
+   			elseif group == "tab2" then
+      			createProfilesTab(self, container);
+   			end
+		end);
+
+		tab:SetTabs({{text="Options", value="tab1"}, {text="Profiles", value="tab2"}});
+		tab:SelectTab("tab1");
+		tab:SetFullWidth(true);
+
+		frame:AddChild(tab);
 	end
 end
 
